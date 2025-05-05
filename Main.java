f0b14f8eb8a29eb4aba3e126d03a6ccfd25f725f
AdamFekkah <112909739+AdamFekkah@users.noreply.github.com>
2025-05-05 05:11:04 +0000

    skibidi yurr

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        
+        String[] wordNums = {"one", "two", "three"};
+        WordPairList exampleOne = new WordPairList(wordNums);
+        System.out.println(exampleOne.getAllPairs());
+        System.out.println("Matches: " + exampleOne.numMatches());
+
+        String[] phrase = {"the", "more", "the", "merrier"};
+        WordPairList exampleTwo = new WordPairList(phrase);
+        System.out.println(exampleTwo.getAllPairs());
+        System.out.println("Matches: " + exampleTwo.numMatches());
+
+        String[] moreWords = {"the", "red", "fox", "the", "red"};
+        WordPairList exampleThree = new WordPairList(moreWords);
+        System.out.println(exampleThree.getAllPairs());
+        System.out.println("Matches: " + exampleThree.numMatches());
     }
 }
